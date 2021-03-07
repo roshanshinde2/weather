6f91b6dad70b4eac6b4e00c595fe476ec116c2c0
roshanshinde2 <34685290+roshanshinde2@users.noreply.github.com>
2021-03-08 01:44:47 +0530

    Update MainActivity.kt
    added comment

--- app/src/main/java/com/ow/openweather/MainActivity.kt
+++ app/src/main/java/com/ow/openweather/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
-
+//home screen
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
